d89fb68288469869bdbc2a69eacec42ccf0c9713
Guillaume <guillaume.taylor@gmail.com>
2016-11-10 16:10:29 +0100

    fuzzy

--- pendule.c
+++ pendule.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
- 
+
+#define ANGLE_MAX 90
+
+#define NB 0
+#define NM 1
+#define NS 2
+#define ZE 3
+#define PS 4
+#define PM 5
+#define PB 6
+
+int fuzzy(float data){
+	if(data==0.0){
+		return ZE;
+	}else if(data<0){
+        	if(data<=ANGLE_MAX*(2/3)*(-1)){
+                	return NB;
+        	}else if(data<=ANGLE_MAX*(1/3)*(-1)){
+                	return NM;
+        	}else{
+			return NS;
+		}
+	}else{
+        	if(data>=ANGLE_MAX*(2/3)){
+        	        return PB;
+        	}else if(data>=ANGLE_MAX*(1/3)){
+        	        return PM;
+        	}else{
+			return PS;
+		}
+	}	
+}
 
 int read_csv(){
 	FILE *fichier;
